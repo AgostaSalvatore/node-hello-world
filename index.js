db73ae87c0ce687aad2b70a6cb8fb15879f8fa38
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-15 13:39:05 +0200

    [UPD] add comment in index.js section

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const worldOne = process.argv[2];
 console.log(`Hello ${worldOne}`);
 
 //eseguito con node index.js point 1
-//eseguito con npm
+//eseguito con npm run start point 2
+//eseguito con npm run watch che aggiorna il server in tempo reale point 3
+//bonus
+//npm run start "Boolean"
